d733d8a34beb0825726e3cae1d6fdd0131f437dc
userkey <5314175+userkey@user.noreply.gitee.com>
2026-01-15 09:22:30 +0800

    优化星海日常放置摆饰，放置后收回

--- whimbox/task/daily_task/starsea_task/place_item_task.py
+++ whimbox/task/daily_task/starsea_task/place_item_task.py
@@ -7,6 +7,7 @@ from whimbox.common.keybind import keybind
 class PlaceItemTask(TaskTemplate):
     def __init__(self):
         super().__init__("place_item_task")
+        self.chose_lantern = False
 
     @register_step("选择摆饰")
     def step1(self):
@@ -24,6 +25,7 @@ class PlaceItemTask(TaskTemplate):
 
         if wait_until_appear_then_click(ButtonItemLanternConfirm, retry_time=2):
             # 选中了河灯摆饰
+            self.chose_lantern = True
             time.sleep(0.5)
             scroll_find_click(AreaDialog, "确认", str_match_mode=0, need_scroll=False)
             itt.delay(0.5, comment="等待退出摆饰选择界面")
@@ -58,6 +60,25 @@ class PlaceItemTask(TaskTemplate):
         if retry_time == 0:
             self.update_task_result(status=STATE_TYPE_FAILED, message="摆饰放置失败")
             itt.right_click()
+
+    @register_step("收回摆饰")
+    def step3(self):
+        if not self.chose_lantern:
+            itt.key_down(keybind.KEYBIND_ITEM)
+            time.sleep(3)
+            itt.key_up(keybind.KEYBIND_ITEM)
+            if not wait_until_appear_then_click(ButtonItemSetting):
+                raise Exception("未找到物品设置按钮")
+            if not wait_until_appear_then_click(ButtonItemFinishSetting):
+                raise Exception("未找到完成设置按钮")
+            if not wait_until_appear_then_click(ButtonItemPlaceableItem):
+                raise Exception("未找到摆饰按钮")
+            itt.delay(0.5, comment="等待摆饰界面加载完成")
+            AreaItemFirstItem.click()
+            itt.move_and_click((0, 0)) #点一下屏幕，退出摆饰界面
+        else:
+            self.log_to_gui("放置的摆饰是河灯，无法收回")
+        
 
 
 if __name__ == "__main__":
